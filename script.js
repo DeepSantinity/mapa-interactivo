e8e3444c752e4a5456ce033d4dcd1db726493e25
DeepSantinity <santymunoz850@gmail.com>
2026-04-01 21:08:59 -0300

    ejercicio 7

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var map = L.map('map').setView([-34.6037, -58.3816], 13);
+let puntos = [];
+let poligonoActual = null;
+let marcadorInicial = null;
 
+var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: 'OpenStreetMap'
 }).addTo(map);
 
-L.marker([-34.6037, -58.3816])
+marcadorInicial = L.marker([-34.6037, -58.3816])
    .addTo(map)
    .bindPopup("Ubicación inicial")
    .openPopup();
@@ -13,11 +16,13 @@ L.marker([-34.6037, -58.3816])
 map.on('click', function(e) {
    puntos.push([e.latlng.lat, e.latlng.lng]);
 
-
    if (puntos.length === 4) {
-       L.polyline(puntos, { color: 'blue' }).addTo(map);
+       poligonoActual = L.polygon(puntos, { color: 'blue' }).addTo(map);
    }
 });
 
-
-let puntos = [];
+document.getElementById("boton").onclick = function() {
+   if (poligonoActual) map.removeLayer(poligonoActual);
+   if (marcadorInicial) map.removeLayer(marcadorInicial);
+   puntos = [];
+};
