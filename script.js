e2155644cab55b28dc27f2396b0be485bbf6f2de
DeepSantinity <santymunoz850@gmail.com>
2026-04-30 20:56:33 -0300

    agrego funcionalidad que calcule y muestre la distancia

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let puntos = [];
 let poligonoActual = null;
 let marcadorInicial = null;
-
+let puntosDistancia = [];
+let lineaDistancia = null;
 // CAPAS
 var light = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
@@ -46,14 +47,37 @@ marcadorInicial = L.marker([-34.6037, -58.3816], {icon: greenIcon})
 map.on('click', function(e) {
   puntos.push([e.latlng.lat, e.latlng.lng]);
 
+  // POLÍGONO (tu lógica actual)
   if (puntos.length === 4) {
     poligonoActual = L.polygon(puntos, { color: 'blue' }).addTo(map);
   }
-});
 
+  // DISTANCIA
+  puntosDistancia.push(e.latlng);
+
+  if (puntosDistancia.length === 2) {
+    let distancia = map.distance(puntosDistancia[0], puntosDistancia[1]);
+
+    // dibuja línea entre puntos
+    if (lineaDistancia) map.removeLayer(lineaDistancia);
+    lineaDistancia = L.polyline(puntosDistancia, { color: 'red' }).addTo(map);
+
+    // mostrar distancia
+    L.popup()
+      .setLatLng(puntosDistancia[1])
+      .setContent("Distancia: " + (distancia / 1000).toFixed(2) + " km")
+      .openOn(map);
+
+    // reset para siguiente medición
+    puntosDistancia = [];
+  }
+  
+});
 // BOTÓN RESET
 document.getElementById("boton").onclick = function() {
   if (poligonoActual) map.removeLayer(poligonoActual);
   if (marcadorInicial) map.removeLayer(marcadorInicial);
   puntos = [];
+  if (lineaDistancia) map.removeLayer(lineaDistancia);
+puntosDistancia = [];
 };
